ee2948c44092afad8e88047ebfafffdbe04e98aa
Mateus Costa dos Santos <soutecinformatica1@gmail.com>
2020-12-18 12:24:09 -0300

    first Commit

---
+++ src/App.js
@@ -0,0 +1,9 @@
+import './App.css';
+
+function App() {
+  return (
+   <h2>OLÁ</h2>
+  );
+}
+
+export default App;
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { BrowserRouter, Route } from 'react-router-dom';
+
+
+export function Routes(){
+    return(
+        <BrowserRouter>
+            <Route path="/lang" />
+        </BrowserRouter>
+    )
+}
