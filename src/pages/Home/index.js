f42865c9e7c8c4052985638a26e5ff3b2a3c0e87
Mateus Costa dos Santos <soutecinformatica1@gmail.com>
2021-02-03 11:22:14 -0300

    Alterações Novas

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,9 +1,8 @@
 import { Link, Redirect } from "react-router-dom";
 import logo from '../../assets/Vector.svg';
-import ImageSlider from "../../components/imageSlider";
-import { SliderData } from "../../components/SliderData";
-import { MdSettings } from "react-icons/md";
 import "./index.css";
+import "./navbar.css"
+
 
 function Home() {
     let lang = localStorage.getItem('lang');
@@ -11,55 +10,90 @@ function Home() {
     if(lang === null){
         return <Redirect to="/lang" />
     }
-   return(
-    <div className="Home">
-    <div className="Navbar">
-        <ul className="list-menu">
-            <img src={logo} alt="Logo do Projeto" id="logo" />
-            <li id="menu_geo">GEOTURISMO</li>
-            <li id="menu_expo">EXPLORAR</li>
-            <li id="menu_territ">NOSSO TERRITÓRIO</li>
-            <li id="menu_publi">PUBLICAÇÕES</li>
-            <li>
-                <div className="settings">
-                    <MdSettings size={25} id="settings" color="#fff" />
-                </div>
-            </li>
-        </ul>
-    </div>
-    <div className="slider">
-        <ImageSlider slides={SliderData} />
-    </div>
-        <div className="carrosuel">
-          <div className="card b-1">
-                <h2>Locais de Interesse</h2>
-                <p>Capital do Estado da Paraíba, João Pessoa possui um dos centros históricos mais antigos do país, repleto de igrejas, monumentos, praças históricas, obeliscos e fontes.</p>
-                <Link to="/en">
-                    <strong>Visitar</strong>
-                </Link>
 
-              
-          </div>
-          <div className="card b-2">
-                <h2>Sobre o Centro Histórico</h2>
-                <p>Capital do Estado da Paraíba, João Pessoa possui um dos centros históricos mais antigos do país, repleto de igrejas, monumentos, praças históricas, obeliscos e fontes.</p>
-                <Link to="/en">
-                    <strong>Visitar</strong>
-                </Link>
-          </div>
-          <div className="card b-3">
-                <h2>Como chegar aqui</h2>
-                <p>Situado no extremo oriental das Américas, João Pessoa é facilmente acessado através do Aeroporto Castro Pinto, distante 10 Km e pelo Terminal Rodoviário Severino Camelo, no centro de João Pessoa.</p>
-                <Link to="/en">
-                    <strong>Visitar</strong>
-                </Link>
-          </div>
+    
+   return(
+        <div className="Home">
+            <header className="header-area overlay">
+                <nav className="navbar navbar-expand-md navbar-dark">
+                    <div className="container">
+                        <Link to="/" className="navbar-brand"><img src={logo} alt="Logo" className="logo"/></Link>
+                        
+                        <button type="button" className="navbar-toggler collapsed" data-toggle="collapse" data-target="#main-nav">
+                            <span className="menu-icon-bar"></span>
+                            <span className="menu-icon-bar"></span>
+                            <span className="menu-icon-bar"></span>
+                        </button>
+                        
+                        <div id="main-nav" className="collapse navbar-collapse">
+                            <ul className="navbar-nav ml-auto">
+                                <li><Link href="#" className="nav-item nav-link active">Geoturismo</Link></li>
+                                <li><Link href="#" className="nav-item nav-link">Explorar</Link></li>
+                                <li className="dropdown">
+                                    <Link href="#" class="nav-item nav-link" data-toggle="dropdown">Nosso Território</Link>
+                                    <div class="dropdown-menu">
+                                        <Link href="#" className="dropdown-item">Dropdown Item 1</Link>
+                                        <Link href="#" className="dropdown-item">Dropdown Item 2</Link>
+                                        <Link href="#" className="dropdown-item">Dropdown Item 3</Link>
+                                    </div>
+                                </li>
+                                <li class="dropdown">
+                                    <Link className="nav-item nav-link" data-toggle="dropdown">Publicações</Link>
+                                    <div class="dropdown-menu">
+                                        <Link href="#" className="dropdown-item">Dropdown Item 1</Link>
+                                        <Link href="#" className="dropdown-item">Dropdown Item 2</Link>
+                                        <Link href="#" className="dropdown-item">Dropdown Item 3</Link>
+                                        <Link href="#" className="dropdown-item">Dropdown Item 4</Link>
+                                        <Link href="#" className="dropdown-item">Dropdown Item 5</Link>
+                                    </div>
+                                </li>
+                                <li><Link href="#" className="nav-item nav-link">Sobre o projeto</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                </nav>
+                
+                <div className="banner">
+                    <div className="container">
+                        <h1>Roteiro Geoturistico do Centro Histórico do João Pessoa</h1>
+                    </div>
+                </div>
+            </header>
+            
+            <div className="container">
+                <div className="blocos">
+                    <div className="bloco-1">
+                        <h2>Locais de Interesse</h2>
+                        <p>Capital do Estado da Paraíba, João Pessoa possui um dos centros históricos mais antigos do país, repleto de igrejas, monumentos, praças históricas, obeliscos e fontes.</p>
+                        <Link to="/en">
+                            <strong>Visitar</strong>
+                        </Link>
+                    </div>
+                    <div className="bloco-2">
+                        <h2>Sobre o Centro Histórico</h2>
+                        <p>Capital do Estado da Paraíba, João Pessoa possui um dos centros históricos mais antigos do país, repleto de igrejas, monumentos, praças históricas, obeliscos e fontes.</p>
+                        <Link to="/en">
+                            <strong>Visitar</strong>
+                        </Link>
+                    </div>
+                    <div className="bloco-3">
+                        <h2>Como chegar aqui</h2>
+                        <p>Situado no extremo oriental das Américas, João Pessoa é facilmente acessado através do Aeroporto Castro Pinto, distante 10 Km e pelo Terminal Rodoviário Severino Camelo, no centro de João Pessoa.</p>
+                        <Link to="/en">
+                            <strong>Visitar</strong>
+                        </Link>
+                    </div>
+                </div>
+            </div>
+                <div className="container">
+                <div className="bottom-footer">
+                    <button type="button" class="btn btn-info btn-lg">Envie um e-mail</button>
+                    <span className="visit">000 <span>Visitas ao site</span></span>
+                </div>
+            </div>
         </div>
-        <div className="buttons-footer">
-            <div className="btn-1"></div>
-            <div className="btn-2"></div>
-        </div>
-    </div>
+   
+
     )
     
 }
