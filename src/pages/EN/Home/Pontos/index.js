c050ab48b0b0307c6ecdf24d5075d1a24465a2da
EnriqueeXD <enrique.sousa@academico.ifpb.edu.br>
2021-03-08 02:08:47 -0300

    Atualizações do projeto em Inglês

---
+++ src/pages/EN/Home/Pontos/index.js
@@ -0,0 +1,13 @@
+import Locais from "../../../components/Locais";
+import NavBar from "../../../components/Navbar";
+
+function Pontos(){
+    return(
+        <>
+        <NavBar />
+        <Locais />
+        </>
+    )
+}
+
+export default Pontos
---
+++ src/pages/EN/components/Navbar/index.js
@@ -0,0 +1,61 @@
+import logo from '../../assets/Vector.svg';
+import { Link } from "react-router-dom";
+import ModalCriterios from "../Modal";
+import "./index.css";
+
+function NavBar(){
+    return(
+        <>
+        <nav className="navbar navbar-expand-md" id="menu">
+            <div className="container">
+            <Link className="navbar-brand" to="/"><img src={logo} id="logo" alt="logo" /></Link>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="material-icons">
+                menu
+            </span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav ml-auto meu-menu">
+                                <li><Link to="/geoturism" className="nav-item nav-link">Geotourism</Link></li>
+                                <li className="dropdown">
+                                    <span className="nav-link" data-toggle="dropdown">Explore</span>
+                                    <div className="dropdown-menu">
+                                        <button className="dropdown-item" data-toggle="modal" data-target="#exampleModal">Criteria</button>
+                                        <Link to="/locais" className="dropdown-item">Places Of Interest</Link>
+                                        <Link to="/" className="dropdown-item">Files</Link>                                        
+                                        <Link to="/" className="dropdown-item">Thematic Exploration</Link>                                        
+                                    </div>
+                                </li>
+                                <li className="dropdown">
+                                    <span className="nav-link" data-toggle="dropdown">Our Territory</span>
+                                    <div className="dropdown-menu">
+                                        <Link to="/" className="dropdown-item">Dropdown Item 1</Link>
+                                        <Link to="/" className="dropdown-item">Dropdown Item 2</Link>
+                                        <Link to="/" className="dropdown-item">Dropdown Item 3</Link>
+                                    </div>
+                                </li>
+                                <li className="dropdown">
+                                    <span className="nav-link" data-toggle="dropdown">Publishing</span>
+                                    <div className="dropdown-menu">
+                                        <Link to="/" className="dropdown-item">Dropdown Item 1</Link>
+                                        <Link to="/" className="dropdown-item">Dropdown Item 2</Link>
+                                        <Link to="/" className="dropdown-item">Dropdown Item 3</Link>
+                                        <Link to="/" className="dropdown-item">Dropdown Item 4</Link>
+                                        <Link to="/" className="dropdown-item">Dropdown Item 5</Link>
+                                    </div>
+                                </li>
+                                <li><Link to="/" className="nav-link">About the Project</Link></li>
+                            </ul>
+                        </div>
+                    </div>
+                    <ModalCriterios />           
+            </nav>
+            <br />
+            <br />
+            <br />
+            <br />
+        </>
+    )
+}
+
+export default NavBar
